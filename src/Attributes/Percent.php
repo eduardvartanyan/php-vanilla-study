b882b8e03fc2fb41addfa8c44d0e0c7b39efc8fa
evartanyan1989 <evartanyan1989@gmail.com>
2025-08-28 18:25:05 +0800

    Добавил атрибут Percent

---
+++ src/Attributes/Percent.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Eduardvartanan\PhpVanilla\Attributes;
+
+#[\Attribute(\Attribute::TARGET_CLASS)]
+class Percent implements Attribute
+{
+    public function __construct()
+    {
+
+    }
+
+    public function validate(mixed $value, string $field): ?string
+    {
+        if (
+            !is_float($value)
+            || $value < 0
+            || $value > 100
+        ) {
+            return "$field: Некорректное значение";
+        }
+        return null;
+    }
+}
