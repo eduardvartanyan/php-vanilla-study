f32ee6c637b9c107cdae23309cc7b7faf6b1fdb4
evartanyan1989 <evartanyan1989@gmail.com>
2025-08-28 18:16:17 +0800

    Добавил трейт HasTimestamps

---
+++ src/Traits/HasTimestamps.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Eduardvartanan\PhpVanilla\Traits;
+
+trait HasTimestamps
+{
+    private \DateTimeImmutable $createdAt;
+    private \DateTimeImmutable $updatedAt;
+
+    public function initTimestamps(): void
+    {
+        $this->createdAt = new \DateTimeImmutable('now');
+    }
+    public function touch(): void
+    {
+        $this->updatedAt = new \DateTimeImmutable('now');
+    }
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+    public function getUpdatedAt(): ?\DateTimeImmutable
+    {
+        return $this->updatedAt;
+    }
+}
