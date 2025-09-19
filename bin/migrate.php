919829cf7477b91ea9bafd6e75b9b5322df4d812
evartanyan1989 <evartanyan1989@gmail.com>
2025-09-19 18:34:57 +0800

    Добавил CRUD-класс UserRepository

--- bin/migrate.php
+++ bin/migrate.php
@@ -4,11 +4,14 @@ require __DIR__ . '/../vendor/autoload.php';
 
 use Eduardvartanan\PhpVanilla\Support\Database;
 
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
+$dotenv->load();
+
 $pdo = Database::pdo();
 $dir = __DIR__ . '/../db/migrations';
 
 foreach (glob($dir . '/*.sql') as $file) {
-    echo '>> Running' . basename($file) . PHP_EOL;
+    echo '>> Running ' . basename($file) . PHP_EOL;
     $pdo->exec(file_get_contents($file));
 }
 echo 'Done.' . PHP_EOL;
---
+++ src/Repository/UserRepository.php
@@ -0,0 +1,64 @@
+<?php
+declare(strict_types=1);
+
+namespace Eduardvartanan\PhpVanilla\Repository;
+
+use PDO;
+use Eduardvartanan\PhpVanilla\Support\Database;
+
+final class UserRepository
+{
+    private PDO $pdo;
+
+    public function __construct()
+    {
+        $this->pdo = Database::pdo();
+    }
+
+    /**
+     * @return int — id добавленного пользователя
+     */
+    public function create(string $name, string $email, int $age): int
+    {
+        $stmt = $this->pdo->prepare("INSERT INTO users (name, email, age) VALUES (:name, :email, :age);");
+        $stmt->execute([':name' => $name, ':email' => $email, ':age' => $age]);
+        return (int) $this->pdo->lastInsertId();
+    }
+
+    public function find(int $id): ?array
+    {
+        $stmt = $this->pdo->prepare("SELECT * FROM users WHERE id = :id;");
+        $stmt->execute([':id' => $id]);
+        $row = $stmt->fetch();
+        return $row ?: null;
+    }
+
+    public function list(int $limit = 10, int $offset = 0): ?array
+    {
+        $stmt = $this->pdo->prepare("SELECT * FROM users ORDER BY id LIMIT :limit OFFSET :offset;");
+        $stmt->bindValue(':limit', $limit, PDO::PARAM_INT);
+        $stmt->bindValue(':offset', $offset, PDO::PARAM_INT);
+        $stmt->execute();
+        $rows = $stmt->fetchAll();
+        return $rows ?: null;
+    }
+
+    public function update(int $id, array $data): bool
+    {
+        if (
+            !array_key_exists('name', $data)
+            || !array_key_exists('email', $data)
+            || !array_key_exists('age', $data)
+        ) { return false; }
+
+        $stmt = $this->pdo->prepare("UPDATE users SET name = :name, email = :email, age = :age WHERE id = :id;");
+
+        return $stmt->execute([':id' => $id, ':name' => $data['name'],':email' => $data['email'], ':age' => $data['age']]);
+    }
+
+    public function delete(int $id): bool
+    {
+        $stmt = $this->pdo->prepare("DELETE FROM users WHERE id = :id");
+        return $stmt->execute([':id'=>$id]);
+    }
+}
