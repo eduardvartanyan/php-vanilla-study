3c8cce64f66742fda9a7a1c0561082a9f01b44fa
evartanyan1989 <evartanyan1989@gmail.com>
2025-09-25 10:46:49 +0800

    Добавил интерфейс AuthStorageInterface

---
+++ src/Contracts/AuthStorageInterface.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+
+namespace Eduardvartanan\PhpVanilla\Contracts;
+
+interface AuthStorageInterface
+{
+    public function setToken(string $token, \DateTimeImmutable $expires): void;
+    public function getToken(): ?string;
+    public function clear(): void;
+}
