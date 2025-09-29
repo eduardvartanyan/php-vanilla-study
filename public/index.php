f4cf757949ea48902f383a95942d705c38548e92
evartanyan1989 <evartanyan1989@gmail.com>
2025-09-29 14:38:31 +0800

    Перевел index.php от Singleton к DI

--- public/index.php
+++ public/index.php
@@ -13,7 +13,6 @@ use Eduardvartanan\PhpVanilla\Http\Controllers\AuthController;
 use Eduardvartanan\PhpVanilla\Repository\PdoSessionRepository;
 use Eduardvartanan\PhpVanilla\Support\BcryptPasswordHasher;
 use Eduardvartanan\PhpVanilla\Support\Container;
-use Eduardvartanan\PhpVanilla\Support\Database;
 use Eduardvartanan\PhpVanilla\Support\RandomTokenGenerator;
 use Eduardvartanan\PhpVanilla\Support\SessionCookieStorage;
 
@@ -21,10 +20,20 @@ $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
 $dotenv->load();
 
 $container = new Container();
+$container->set(PDO::class, fn() => new PDO(
+    $_ENV['DB_DSN'],
+    $_ENV['DB_USER'],
+    $_ENV['DB_PASS'],
+    [
+        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES   => false,
+    ]
+));
 $container->set(AuthStorageInterface::class, fn() => new SessionCookieStorage());
 $container->set(CsrfTokenManagerInterface::class, fn() => new CsrfTokenManager());
 $container->set(PasswordHasherInterface::class, fn() => new BcryptPasswordHasher());
-$container->set(SessionRepositoryInterface::class, fn($container) => new PdoSessionRepository(Database::pdo()));
+$container->set(SessionRepositoryInterface::class, fn($container) => new PdoSessionRepository($container->get(PDO::class)));
 $container->set(TokenGeneratorInterface::class, fn() => new RandomTokenGenerator());
 
 try {
